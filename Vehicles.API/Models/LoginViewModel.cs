050812b998856331bf95f9fa0022552ccd19a41d
oswaldofh <oswaldofuentes249734@correo.itm.edu.co>
2021-09-04 14:48:15 -0500

    Implement login and logout

---
+++ Vehicles.API/Models/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Models
+{
+    public class LoginViewModel
+    {
+
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Debes ingresar un Email valido")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        [MaxLength(6, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordrme")]
+        public bool RememberMe { get; set; }
+    }
+}
--- Vehicles.API/Program.cs
+++ Vehicles.API/Program.cs
@@ -1,20 +1,14 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Vehicles.API.Data;
 
 namespace Vehicles.API
 {
     public class Program
     {
-        
+
         public static void Main(string[] args)
         {
             IWebHost host = CreateWebHostBuilder(args).Build();
